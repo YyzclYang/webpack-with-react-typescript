e633669a69d58ce0a1cee7826d045af0ec9314d4
YyzclYang <yyzclyang@gmail.com>
2019-09-06 20:39:16 +0800

    chore：初始化入口文件

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="page">
+      <div>App</div>
+    </div>
+  );
+};
+
+export default App;
