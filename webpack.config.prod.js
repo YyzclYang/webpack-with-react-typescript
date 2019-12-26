7eacf808e84a9f80670acbd24f1ccc9a5b941736
YyzclYang <yyzclyang@gmail.com>
2019-12-26 08:39:58 +0800

    feat(webpack)：添加 gzip

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.config");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
 module.exports = merge(baseConfig, {
   mode: "production",
@@ -33,6 +34,12 @@ module.exports = merge(baseConfig, {
     }
   },
   plugins: [
+    new CompressionWebpackPlugin({
+      algorithm: "brotliCompress", // 压缩算法，Node 11.7.0 对 Brotli 有内置支持
+      test: /\.js(\?.*)?$/i,
+      threshold: 10240, // 大于 10kb 的资源才会压缩
+      minRatio: 0.8 // 压缩比大于 0.8 的资源才会压缩
+    }),
     new MiniCssExtractPlugin({
       filename: "static/css/[name].[hash].css",
       chunkFilename: "[id].css"
